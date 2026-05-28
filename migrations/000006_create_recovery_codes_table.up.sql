19707c23f75455ee554eb36268f35de8392d52ee
Anish Neupane <anishneupane1@gmail.com>
2026-05-28 00:06:55 -0700

    Add Updated by and at

--- migrations/000006_create_recovery_codes_table.up.sql
+++ migrations/000006_create_recovery_codes_table.up.sql
@@ -7,7 +7,13 @@ create table if not exists recovery_codes
     used_at    timestamptz,
 
     created_at timestamptz      not null default now(),
-    created_by uuid             not null
+    created_by uuid             not null,
+
+    updated_at timestamptz      not null default now(),
+    updated_by uuid             not null,
+
+    constraint check_created_updated_at
+        check ( updated_at >= created_at )
 );
 
 create index if not exists idx_recovery_codes_unused
@@ -16,3 +22,6 @@ create index if not exists idx_recovery_codes_unused
 
 create index if not exists idx_recovery_codes_created_by
     on recovery_codes (created_by);
+
+create index if not exists idx_recovery_codes_updated_by
+    on recovery_codes (updated_by);
