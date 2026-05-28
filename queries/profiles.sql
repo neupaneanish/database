157f57075db1d5b61febe135a969dffbb9ae595e
Anish Neupane <anishneupane1@gmail.com>
2026-05-27 23:44:51 -0700

    Profiles Query

---
+++ queries/profiles.sql
@@ -0,0 +1,28 @@
+-- name: CreateProfile :one
+insert into profiles(user_id, name, avatar, about, dob, phone, created_by, updated_by)
+values (@user_id, @name, @avatar, @about, @dob, @phone, @created_by, @updated_by)
+returning *;
+
+-- name: UpdateProfile :one
+update profiles
+set name       = coalesce(sqlc.narg('name'), name),
+    avatar     = coalesce(sqlc.narg('avatar'), avatar),
+    about      = coalesce(sqlc.narg('about'), about),
+    dob        = coalesce(sqlc.narg('dob'), dob),
+    phone      = coalesce(sqlc.narg('phone'), phone),
+    updated_at = now(),
+    updated_by = @updated_by
+where id = @id
+  and user_id = @user_id
+  and updated_at = @updated_at::timestamptz
+  and (coalesce(sqlc.narg('name'), name),
+       coalesce(sqlc.narg('avatar'), avatar),
+       coalesce(sqlc.narg('about'), about),
+       coalesce(sqlc.narg('dob'), dob),
+       coalesce(sqlc.narg('phone'), phone) is distinct from (name, avatar, about, dob, phone))
+returning *;
+
+-- name: Profile: one
+select *
+from profiles
+where user_id = @user_id;
