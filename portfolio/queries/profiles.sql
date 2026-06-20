cb6d2c635cb9f52f60e1ccd8ae379f6e9c4baebd
Anish Neupane <anishneupane1@gmail.com>
2026-06-20 10:21:38 -0700

    Portfolio Profiles

---
+++ portfolio/queries/profiles.sql
@@ -0,0 +1,12 @@
+-- name: CreateProfile :execresult
+insert into profiles (user_id, name, headline, dob, created_by, updated_by)
+values (@user_id, @name, sqlc.narg('headline'), @dob, @created_by, @updated_by);
+
+-- name: UpdateProfile :one
+update profiles
+set headline   = @headline,
+    updated_at = now()
+where user_id = @user_id
+  and updated_at = @updated_at::timestamptz
+  and headline is distinct from @headline
+returning *;
