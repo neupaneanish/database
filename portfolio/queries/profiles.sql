751e7e202651550111935a8d9551371c0d5cf054
Anish Neupane <anishneupane1@gmail.com>
2026-06-22 01:57:48 -0700

    About Query and update_by in profiles.sql

--- portfolio/queries/profiles.sql
+++ portfolio/queries/profiles.sql
@@ -5,7 +5,8 @@ values (@user_id, @name, sqlc.narg('headline'), @dob, @created_by, @updated_by);
 -- name: UpdateProfile :one
 update profiles
 set headline   = @headline,
-    updated_at = now()
+    updated_at = now(),
+    updated_by = @updated_by
 where user_id = @user_id
   and updated_at = @updated_at::timestamptz
   and headline is distinct from @headline
