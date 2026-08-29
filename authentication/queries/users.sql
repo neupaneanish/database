6f53b9b3e8043b081f90e6ce14ca8fb4b2e4ba54
Anish Neupane <anishneupane1@gmail.com>
2026-08-28 23:49:56 -0700

    Update Query

--- authentication/queries/users.sql
+++ authentication/queries/users.sql
@@ -1,7 +1,17 @@
 -- name: CreateUser :one
 insert into users (email, username, phone, role, status, created_by, updated_by)
 values (@email, @username, @phone, @role, @status, @created_by, @updated_by)
-returning *;
+returning id,
+    email,
+    username,
+    phone,
+    role,
+    status,
+    email_verified_at,
+    created_at,
+    created_by,
+    updated_at,
+    updated_by;
 
 -- name: VerifyEmail :execresult
 update users
@@ -30,7 +40,17 @@ set status     = @status,
 where id = @id
   and updated_at = @updated_at
   and @status is distinct from status
-returning *;
+returning id,
+    email,
+    username,
+    phone,
+    role,
+    status,
+    email_verified_at,
+    created_at,
+    created_by,
+    updated_at,
+    updated_by;
 
 -- name: UpdateRole :one
 update users
@@ -40,15 +60,41 @@ set role       = @role,
 where id = @id
   and updated_at = @updated_at
   and @role is distinct from role
-returning *;
+returning id,
+    email,
+    username,
+    phone,
+    role,
+    status,
+    email_verified_at,
+    created_at,
+    created_by,
+    updated_at,
+    updated_by;
 
 -- name: User :one
-select u.*,
-       (exists(select 1 from two_factors tf where tf.user_id = u.id))::boolean as two_factor
+select u.id,
+       u.email,
+       u.username,
+       u.phone,
+       u.role,
+       u.status,
+       u.email_verified_at,
+       u.created_at,
+       u.created_by,
+       u.updated_at,
+       u.updated_by,
+       (exists(select 1 from two_factors tf where tf.user_id = u.id))::boolean as two_factor,
+       c.created_at::timestamptz                                               as last_password_updated_at
 from users u
+         join lateral ( select c.created_at
+                        from credentials c
+                        where c.user_id = u.id
+                        order by c.id desc
+                        limit 1) as c on true
 where u.id = @id;
 
--- name: Users :one
+-- name: Users :many
 select id,
        email,
        username,
