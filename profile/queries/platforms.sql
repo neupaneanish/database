f75377ded028212d93c2d90e83dc6e50199c22c0
Anish Neupane <anishneupane1@gmail.com>
2026-09-02 23:56:56 -0700

    rename prefix to url_suffix

--- profile/queries/platforms.sql
+++ profile/queries/platforms.sql
@@ -1,6 +1,6 @@
 -- name: CreatePlatform :one
-insert into platforms (name, url, logo_url, color, prefix, created_by, updated_by)
-values (@name, @url, @logo_url, @color, @prefix, @created_by, @updated_by)
+insert into platforms (name, url, logo_url, color, url_suffix, created_by, updated_by)
+values (@name, @url, @logo_url, @color, @url_suffix, @created_by, @updated_by)
 returning id;
 
 -- name: UpdatePlatform :one
@@ -9,12 +9,12 @@ set name       = @name,
     url        = @url,
     logo_url   = @logo_url,
     color      = @color,
-    prefix     = @prefix,
+    url_suffix = @url_suffix,
     updated_at = now(),
     updated_by = @updated_by
 where id = @id
   and updated_at = @updated_at::timestamptz
-  and (name, url, logo_url, color, prefix) is distinct from (@name, @url, @logo_url, @color, @prefix)
+  and (name, url, logo_url, color, url_suffix) is distinct from (@name, @url, @logo_url, @color, @url_suffix)
 returning id;
 
 -- name: Platform :one
@@ -23,7 +23,7 @@ from platforms
 where id = @id;
 
 -- name: Platforms :many
-select id, name, logo_url, color, prefix
+select id, name, logo_url, color, url_suffix
 from platforms
 order by name;
 
